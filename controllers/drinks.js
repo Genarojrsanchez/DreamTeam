20575595354a372509b4c72af77add4861bbdad3
Genaro Sanchez <genarorwc@yahoo.com>
2020-03-25 14:09:18 -0700

    added controller && updated session.js

--- controllers/drinks.js
+++ controllers/drinks.js
@@ -4,15 +4,33 @@ const Drinks = require('../models/drinks.js');
 
 //GET
 router.get("/", (req, res) => {
-  res.send("hello world");
+  Drinks.find({}, (err, foundDrinks) => {
+    res.json(foundDrinks);
+  })
 })
 
 //POST
 router.post("/", (req, res) => {
-  Drinks.create(req.body)
+  Drinks.create(req.body, (err, createdDrinks)
+  => {
+    res.json(createdDrinks)
+  })
 })
+
 //PUT
 
+router.put("/:id", (req, res) => {
+  Drinks.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updatedDrinks) =>{
+    res.json(updatedDrinks);
+  })
+})
+
 //DELETE
+
+router.put("/:id", (req, res) => {
+  Drinks.findByIdAndRemove(req.params.id, (err, deletedDrinks) => {
+    res.json(deletedDrinks);
+  })
+})
 
 module.exports = router;
