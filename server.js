746533b9c83b60be25911a376414e76764c125da
Genaro Sanchez <genarorwc@yahoo.com>
2020-03-25 00:36:19 -0700

    added notes to todo

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const session = require('express-session');
+require("dotenv").config();
+const session = require('express-sessions');
+const db = mongoose.connection;
+
 
 app.use(express.json());
 app.use(express.static('public'));
@@ -17,11 +20,16 @@ app.use(session({
 //
 
 
-mongoose.connect('mongodb://localhost:27017/meancrud', {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/tipsy', {useNewUrlParser:true, useUnifiedTopology: true, useFindAndModify: false});
 mongoose.connection.once('open', () => {
     console.log('connected to mongoose');
 })
 
 app.listen(3000, () => {
-    console.log('listening');
+    console.log(`listening on ${process.env.PORT}`);
 })
+
+
+db.on("error", (err) => (console.log(err.message + "is mongod not running?")));
+db.on("connected", () => (console.log("mongo connected: to the URL")));
+db.on("disconnected", () => (console.log("mongo disconnected")));
