c7ce49f6cffe524d8080a172bcd99526e048ebc3
Genaro Sanchez <genarorwc@yahoo.com>
2020-03-26 12:11:52 -0700

    session && user controller updated

--- controllers/session.js
+++ controllers/session.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users.js');
+const bycrpt = require("bcryptjs");
 //bcrypt route - need to figure out how to deal with since Z has to use bcrptyjs
 
 // ===RouterRoute(POST)====
-
+router.post("/", (req, res) => {
+  User.findOne({username:req.body.username}, (error, foundUser) =>{
+    if(foundUser === null){
+          res.json({
+              message:'user not found',
+          });
+      } else {
+          const passwordMatch = bcrypt.compareSync(req.body.password, foundUser.password);
+          if(passwordMatch){
+            req.session.user = foundUser;
+              res.json(foundUser)
+          } else {
+              res.json({
+                  message:'incorrect password'
+              });
+          }
+      }
+  });
+});
 
 // ======GetSessionRoute======
 router.get("/", (req, res) => {
--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users.js');
+const bcrypt = require("bcryptjs");
 //bcrypt route - need to figure out how to deal with since Z has to use bcrptyjs
-
+router.post("/", (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password,
+  bcrypt.genSaltSync(10));
+  User.create(req.body, (error, createdUser) => {
+    req.session.user = createdUser;
+    res.json(createdUser)
+  })
+})
 
 
 module.exports = router;
